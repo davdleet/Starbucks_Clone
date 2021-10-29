8ff7e4e24c882b796db1464191c37f8afa5ea166
davdleet <davdleet1@gmail.com>
2021-10-29 22:40:23 +0900

    adding images

--- js/main.js
+++ js/main.js
@@ -15,3 +15,19 @@ searchInputEl.addEventListener("blur", ()=> {
     searchInputEl.setAttribute('placeholder', '');
 });
 
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(() => {
+    console.log(window.scrollY)
+    if(window.scrollY > 500){
+        gsap.to(badgeEl, 0.6, {
+            opacity: 0,
+            display: "none"
+        });
+    }else{
+        gsap.to(badgeEl, 0.6, {
+            opacity: 1,
+            display: "block"
+        });
+    }
+}, 300))
