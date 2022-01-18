cc61d556f34ad8576a6f1971b0a137ea702592c6
davdleet <davdleet1@gmail.com>
2022-01-18 21:13:29 +0900

    adding more sections and animations

--- js/main.js
+++ js/main.js
@@ -18,7 +18,6 @@ searchInputEl.addEventListener("blur", ()=> {
 const badgeEl = document.querySelector('header .badges');
 
 window.addEventListener('scroll', _.throttle(() => {
-    console.log(window.scrollY)
     if(window.scrollY > 500){
         gsap.to(badgeEl, 0.6, {
             opacity: 0,
@@ -62,4 +61,46 @@ new Swiper('.promotion .swiper-container', {
         prevEl: '.promotion .swiper-prev',
         nextEl: '.promotion .swiper-next'
     }
+})
+
+const promotionEl = document.querySelector('.promotion')
+const promotionToggleBtn = document.querySelector('.toggle-promotion')
+let isHidePromotion = false
+promotionToggleBtn.addEventListener('click', function(){
+    isHidePromotion = !isHidePromotion
+    if(isHidePromotion){
+        promotionEl.classList.add('hide')
+    }
+    else{
+        promotionEl.classList.remove('hide')
+    }
+})
+
+
+function random(min, max){
+    return parseFloat((Math.random() * (max *min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size){
+    gsap.to(selector, random(1.5, 2.5), {
+        y: size,
+        repeat: -1,
+        yoyo: true,
+        ease: Power1.easeInOut,
+        delay: random(0, delay)
+    });
+}
+floatingObject('.floating1', 1, 15)
+floatingObject('.floating2', .5, 15)
+floatingObject('.floating3', 1.5, 20)
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+    new ScrollMagic
+        .Scene({
+            triggerElement: spyEl,
+            triggerHook: .8
+        })
+        .setClassToggle(spyEl, 'show')
+        .addTo(new ScrollMagic.Controller())
 })
